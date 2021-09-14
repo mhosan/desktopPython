29f772ac9794db0d8707c016f52b0244f1d95a81
marcelo <mhosan@gmail.com>
2021-09-14 10:09:59 -0300

    cambiando a libreta de contactos

--- part_manager.py
+++ part_manager.py
@@ -10,12 +10,12 @@ def populate_list():
         parts_list.insert(END, row)
 
 def add_item():
-    if part_text.get() == '' or customer_text.get() == '' or retailer_text.get() == '' or price_text.get() == '':
+    if apellido.get() == '' or nombre.get() == '' or email.get() == '' or celular.get() == '':
         messagebox.showerror('Campos requeridos', 'Llenar todos los datos')
         return
-    db.insert(part_text.get(), customer_text.get(), retailer_text.get(), price_text.get())
+    db.insert(apellido.get(), nombre.get(), email.get(), celular.get())
     parts_list.delete(0, END)
-    parts_list.insert(END, (part_text.get(), customer_text.get(), retailer_text.get(), price_text.get()))
+    parts_list.insert(END, (apellido.get(), nombre.get(), email.get(), celular.get()))
     clear_text()
     populate_list()
 
@@ -24,14 +24,14 @@ def select_item(event):
     index = parts_list.curselection()[0]
     selected_item = parts_list.get(index)
     
-    part_entry.delete(0, END)
-    part_entry.insert(END, selected_item[1])
-    customer_entry.delete(0, END)
-    customer_entry.insert(END, selected_item[2])
-    retailer_entry.delete(0, END)
-    retailer_entry.insert(END, selected_item[3])
-    price_entry.delete(0, END)
-    price_entry.insert(END, selected_item[4])
+    apellido_entry.delete(0, END)
+    apellido_entry.insert(END, selected_item[1])
+    nombre_entry.delete(0, END)
+    nombre_entry.insert(END, selected_item[2])
+    email_entry.delete(0, END)
+    email_entry.insert(END, selected_item[3])
+    celular_entry.delete(0, END)
+    celular_entry.insert(END, selected_item[4])
 
 
 def remove_item():
@@ -40,41 +40,41 @@ def remove_item():
     populate_list()
 
 def update_item():
-    db.update(selected_item[0], part_text.get(), customer_text.get(), retailer_text.get(), price_text.get())
+    db.update(selected_item[0], apellido.get(), nombre.get(), email.get(), celular.get())
     populate_list()
 
 def clear_text():
-    part_entry.delete(0, END)
-    customer_entry.delete(0, END)
-    retailer_entry.delete(0, END)
-    price_entry.delete(0, END)
+    apellido_entry.delete(0, END)
+    nombre_entry.delete(0, END)
+    email_entry.delete(0, END)
+    celular_entry.delete(0, END)
 
 
 app = Tk()
 
-part_text = StringVar()
-part_label = Label(app, text='Nombre parte', font=('bold',14), pady=20)
-part_label.grid(row=0, column=0, sticky=W)
-part_entry = Entry(app, textvariable=part_text)
-part_entry.grid(row=0, column=1)
+apellido = StringVar()
+apellido_label = Label(app, text='Apellido', font=('bold',14), pady=20)
+apellido_label.grid(row=0, column=0, sticky=W)
+apellido_entry = Entry(app, textvariable=apellido)
+apellido_entry.grid(row=0, column=1)
 
-customer_text = StringVar()
-customer_label = Label(app, text='customer', font=('bold',14))
-customer_label.grid(row=0, column=2, sticky=W)
-customer_entry = Entry(app, textvariable=customer_text)
-customer_entry.grid(row=0, column=3)
+nombre = StringVar()
+nombre_label = Label(app, text='Nombre', font=('bold',14))
+nombre_label.grid(row=0, column=2, sticky=W)
+nombre_entry = Entry(app, textvariable=nombre)
+nombre_entry.grid(row=0, column=3)
 
-retailer_text = StringVar()
-retailer_label = Label(app, text='retailer', font=('bold',14))
-retailer_label.grid(row=1, column=0, sticky=W)
-retailer_entry = Entry(app, textvariable=retailer_text)
-retailer_entry.grid(row=1, column=1)
+email = StringVar()
+email_label = Label(app, text='email', font=('bold',14))
+email_label.grid(row=1, column=0, sticky=W)
+email_entry = Entry(app, textvariable=email)
+email_entry.grid(row=1, column=1)
 
-price_text = StringVar()
-price_label = Label(app, text='price', font=('bold',14))
-price_label.grid(row=1, column=2, sticky=W)
-price_entry = Entry(app, textvariable=price_text)
-price_entry.grid(row=1, column=3)
+celular = StringVar()
+celular_label = Label(app, text='Celular', font=('bold',14))
+celular_label.grid(row=1, column=2, sticky=W)
+celular_entry = Entry(app, textvariable=celular)
+celular_entry.grid(row=1, column=3)
 
 parts_list = Listbox(app, height=8, width=50, border=0)
 parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
@@ -86,16 +86,16 @@ parts_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=parts_list.yview)
 parts_list.bind('<<ListboxSelect>>', select_item)
 
-add_btn=Button(app, text='Add Part', width=12, command=add_item)
+add_btn=Button(app, text='Agregar contacto', width=12, command=add_item)
 add_btn.grid(row=2, column=0, pady=20)
 
-remove_btn=Button(app, text='Remove Part', width=12, command=remove_item)
+remove_btn=Button(app, text='Borrar contacto', width=12, command=remove_item)
 remove_btn.grid(row=2, column=1)
 
-update_btn=Button(app, text='Update Part', width=12, command=update_item)
+update_btn=Button(app, text='Actualizar contacto', width=12, command=update_item)
 update_btn.grid(row=2, column=2)
 
-clear_btn=Button(app, text='Clear input', width=12, command=clear_text)
+clear_btn=Button(app, text='Limpiar datos', width=12, command=clear_text)
 clear_btn.grid(row=2, column=3)
 
 
